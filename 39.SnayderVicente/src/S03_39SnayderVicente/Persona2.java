d8d15dd88eeca65aa576840cabf9c878c1b654eb
SnayderVicente <132237250+SnayderVicente@users.noreply.github.com>
2023-10-03 09:40:46 -0500

    Update Persona2.java

--- 39.SnayderVicente/src/S03_39SnayderVicente/Persona2.java
+++ 39.SnayderVicente/src/S03_39SnayderVicente/Persona2.java
@@ -1,23 +1,5 @@
 package S03_39SnayderVicente;
 
 public class Persona2 {
-    private final String nombre;
-    private final int edad;
 
-    public Persona2(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public boolean esMayorDeEdad() {
-        return edad >= 18;
-    }
 }
